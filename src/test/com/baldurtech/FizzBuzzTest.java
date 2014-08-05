4acd5938ae7d7ad56f5931951de4227daef16dfa
baijiayong <jiayongbai@gmail.com>
2014-08-05 10:59:18 +0800

    修改了打印测试结果的代码

--- src/test/com/baldurtech/FizzBuzzTest.java
+++ src/test/com/baldurtech/FizzBuzzTest.java
@@ -1,20 +1,26 @@
 package com.baldurtech;
 
 public class FizzBuzzTest {
+    public static Boolean testResult = true;
+    
     public static void main(String args[]) {
         FizzBuzz fizzBuzz = new FizzBuzz();
         
         new FizzBuzzTest().assertEquals("1", fizzBuzz.say(1));
         new FizzBuzzTest().assertEquals("Fizz",fizzBuzz.say(3));
+        
+        outputTestResult();
         }
     public void assertEquals(String exceptedResult,String actualResult) {
-        if(exceptedResult.equals(actualResult)) {
-            
-        }else {
-            outputTestResult();
+        if(! exceptedResult.equals(actualResult)) {
+            System.out.println("Excepted " + exceptedResult + " But " + actualResult);
         }
     }
-    public void outputTestResult() {
-        System.out.println(false);
+    public static void outputTestResult() {
+        if(testResult){
+            System.out.println("Test Success!");
+        } else {
+            System.out.println("Test Failed!");
+        }
     }
 }
