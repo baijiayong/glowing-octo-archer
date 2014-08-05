9a81a2e863acc9939f3e98ea672a0b412106dadb
baijiayong <jiayongbai@gmail.com>
2014-08-05 10:36:13 +0800

    初始化项目

---
+++ compile.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+echo "Compile"
+
+mkdir -p target/{classes,test-classes}/
+
+javac -d target/classes src/main/com/baldurtech/FizzBuzz.java
+
+javac -d target/test-classes -classpath "target/classes" src/test/com/baldurtech/FizzBuzzTest.java
+
+echo "Compile Done"
---
+++ src/main/com/baldurtech/FizzBuzz.java
@@ -0,0 +1,7 @@
+package com.baldurtech;
+
+public class FizzBuzz {
+    public String say(Integer num) {
+        return String.valueOf(num);
+    }
+}
