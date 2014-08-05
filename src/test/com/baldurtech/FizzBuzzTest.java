e5e03b7ee76a6b539f36f55088905970831a826d
baijiayong <jiayongbai@gmail.com>
2014-08-05 11:13:49 +0800

    将测试案例分别提取为方法

--- src/test/com/baldurtech/FizzBuzzTest.java
+++ src/test/com/baldurtech/FizzBuzzTest.java
@@ -2,16 +2,26 @@ package com.baldurtech;
 
 public class FizzBuzzTest {
     public static Boolean testResult = true;
-    
+    FizzBuzz fizzBuzz = new FizzBuzz();
     public static void main(String args[]) {
-        FizzBuzz fizzBuzz = new FizzBuzz();
         
-        new FizzBuzzTest().assertEquals("1", fizzBuzz.say(1));
-        new FizzBuzzTest().assertEquals("Fizz", fizzBuzz.say(3));
-        new FizzBuzzTest().assertEquals("Buzz", fizzBuzz.say(5));
+        new FizzBuzzTest().test_1_should_be_1();
+        new FizzBuzzTest().test_3_should_be_Fizz();
+        new FizzBuzzTest().test_5_should_be_Buzz();
         
         outputTestResult();
-        }
+    }
+    public void test_1_should_be_1() {
+        assertEquals("1", fizzBuzz.say(1));
+    }
+    
+    public void test_3_should_be_Fizz() {
+        assertEquals("Fizz", fizzBuzz.say(3));
+    }
+    
+    public void test_5_should_be_Buzz() {
+        assertEquals("Buzz", fizzBuzz.say(5));
+    }
     public void assertEquals(String exceptedResult,String actualResult) {
         if(! exceptedResult.equals(actualResult)) {
             testResult = false;
