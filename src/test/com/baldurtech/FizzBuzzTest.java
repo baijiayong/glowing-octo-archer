00ebf5a824aab95be18c85d2f980675296a85dec
baijiayong <jiayongbai@gmail.com>
2014-08-05 10:43:39 +0800

    修改打印结果，打印出 true 和 false

--- src/test/com/baldurtech/FizzBuzzTest.java
+++ src/test/com/baldurtech/FizzBuzzTest.java
@@ -4,7 +4,7 @@ public class FizzBuzzTest {
     public static void main(String args[]) {
         FizzBuzz fizzBuzz = new FizzBuzz();
         
-        System.out.println(fizzBuzz.say(1));
-        System.out.println(fizzBuzz.say(3));
+        System.out.println("1".equals(fizzBuzz.say(1)));
+        System.out.println("Fizz".equals(fizzBuzz.say(3)));
     }
 }
