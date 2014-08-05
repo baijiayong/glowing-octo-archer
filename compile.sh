14e29fb1867e85e79c63a7919522c98806ba73f9
baijiayong <jiayongbai@gmail.com>
2014-08-05 14:47:57 +0800

    修改编译脚本，编译test下面的所有java文件

--- compile.sh
+++ compile.sh
@@ -6,6 +6,6 @@ mkdir -p target/{classes,test-classes}/
 
 javac -d target/classes src/main/com/baldurtech/FizzBuzz.java
 
-javac -d target/test-classes -classpath "target/classes" src/test/com/baldurtech/FizzBuzzTest.java
+javac -d target/test-classes -classpath "target/classes" src/test/com/baldurtech/*.java
 
 echo "Compile Done"
