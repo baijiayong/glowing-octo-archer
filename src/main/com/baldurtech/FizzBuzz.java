216534bfbbad2f2efe451d659d2f4ee979fec686
baijiayong <jiayongbai@gmail.com>
2014-08-05 10:42:08 +0800

    让测试用例3通过

--- src/main/com/baldurtech/FizzBuzz.java
+++ src/main/com/baldurtech/FizzBuzz.java
@@ -2,6 +2,9 @@ package com.baldurtech;
 
 public class FizzBuzz {
     public String say(Integer num) {
+        if(num % 3 == 0) {
+            return "Fizz";
+        }
         return String.valueOf(num);
     }
 }
