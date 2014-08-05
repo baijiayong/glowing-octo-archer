828125929f08a86dab3ac724249988c151bae9a3
baijiayong <jiayongbai@gmail.com>
2014-08-05 11:26:04 +0800

    使用反射动态的获取以test开头的方法并且执行

--- src/test/com/baldurtech/FizzBuzzTest.java
+++ src/test/com/baldurtech/FizzBuzzTest.java
@@ -1,13 +1,20 @@
 package com.baldurtech;
 
+import java.lang.reflect.Method;
+
 public class FizzBuzzTest {
     public static Boolean testResult = true;
     FizzBuzz fizzBuzz = new FizzBuzz();
-    public static void main(String args[]) {
+    public static void main(String args[]) throws Exception {
         
-        new FizzBuzzTest().test_1_should_be_1();
-        new FizzBuzzTest().test_3_should_be_Fizz();
-        new FizzBuzzTest().test_5_should_be_Buzz();
+        Method[] methods = FizzBuzzTest.class.getDeclaredMethods();
+        for(Method method : methods) {
+            
+            if(method.getName().startsWith("test")){
+                Object obj = FizzBuzzTest.class.newInstance();
+                method.invoke(obj, new Object[] {});
+            }
+        }
         
         outputTestResult();
     }
